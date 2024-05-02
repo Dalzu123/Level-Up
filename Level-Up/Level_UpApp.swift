f04f5ff613723ea7bfccbd33955aabc0d69c310e
Dalzu123 <diegoalzugaray@gmail.com>
2024-05-01 21:14:46 -0400

    Initial Commit

---
+++ Level-Up/Level_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Level_UpApp.swift
+//  Level-Up
+//
+//  Created by Diego Alzugaray on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Level_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
